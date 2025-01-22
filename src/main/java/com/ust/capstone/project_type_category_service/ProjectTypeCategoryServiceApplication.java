87819d4f822e3bd75ebed3fe2d73a9eb6ccf7636
Abhinav Sudhakar <28253243+AbhinavSdr@users.noreply.github.com>
2025-01-22 23:11:49 +0530

    initial upload

---
+++ src/main/java/com/ust/capstone/project_type_category_service/ProjectTypeCategoryServiceApplication.java
@@ -0,0 +1,13 @@
+package com.ust.capstone.project_type_category_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectTypeCategoryServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjectTypeCategoryServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ust/capstone/project_type_category_service/ProjectTypeCategoryServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ust.capstone.project_type_category_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjectTypeCategoryServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
